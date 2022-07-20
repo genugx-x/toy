407c23ff4497afd6abf191e93b08943343d115a7
genugx_x <qparanoya@gmail.com>
2022-07-20 19:38:52 +0900

    #1 프로젝트 초기화 및 web 모듈 생성

---
+++ web/src/test/java/com/genug/toy/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genug.toy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
