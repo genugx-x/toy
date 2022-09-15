24587986334dbb97a4727922ed8e01380949e0dd
genugx-x <qparanoya@gmail.com>
2022-09-15 16:44:17 +0900

    #7 마이크로 서비스 코드 수정 및 불필요 리액트 프로젝트 제거

--- licensing-service/src/main/java/com/genug/toy/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/genug/toy/license/LicensingServiceApplication.java
@@ -2,8 +2,10 @@ package com.genug.toy.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class LicensingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LicensingServiceApplication.class, args);
---
+++ licensing-service/src/main/java/com/genug/toy/license/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.genug.toy.license.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+}
