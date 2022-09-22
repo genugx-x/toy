9b21a846a27ad02b6a78126862671b03ab23839d
genugx-x <qparanoya@gmail.com>
2022-09-22 21:47:27 +0900

    #12 Eureka-server 및 eureka client(organization-service) 모듈 추가 - 관련 환경 속성 config-server 반영

--- licensing-service/src/main/java/com/genug/toy/license/repository/LicenseRepository.java
+++ licensing-service/src/main/java/com/genug/toy/license/repository/LicenseRepository.java
@@ -1,12 +1,13 @@
 package com.genug.toy.license.repository;
 
 import com.genug.toy.license.model.License;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface LicenseRepository {
+public interface LicenseRepository extends JpaRepository<License, String> {
     public List<License> findByOrganizationId(String organizationId);
     public License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
 }
